10224f7af3d84a32762aa0c815d11170790c5e84
Vicente Carbonell <vcarbonellf@gmail.com>
2021-12-09 23:00:41 +0100

    Primer commit

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const GifGridItem = ( {title, url} ) => {
+    return (
+        <div className="card animate__animated animate__bounce">
+           <img src={url}  tag={title} />
+           <p>{ title }</p>
+        </div>
+    )
+}
